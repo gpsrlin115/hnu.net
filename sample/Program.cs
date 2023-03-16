ffe83dec126ec883e61c5526a55061a9ab57b184
JinK2133 <20202961@gm.hannam.ac.kr>
2023-03-16 10:27:51 +0900

    Solve 1

--- sample/Program.cs
+++ sample/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("숫자를 입력하세요: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine("입력한 숫자는 양수입니다.");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("입력한 숫자는 음수입니다.");
+        }
+        else
+        {
+            Console.WriteLine("입력한 숫자는 0입니다.");
+        }
+        Console.ReadKey();
+    }
+}
